c951f8bb1d5c585c508ebdbea830b1786d165019
Cristina Hernández <achh17@gmail.com>
2023-02-09 06:44:02 -0500

    I organized the code for improve the order and the logic of the game.

--- script.js
+++ script.js
@@ -1,45 +1,70 @@
-function seleccionaItemJugador() {
-  let inputImages = document.getElementById('images');
-  let inputWords = document.getElementById('words');
-
-  inputImages.addEventListener("click", function () {
-    if (inputImages.checked) {
-      alert("Seleccionaste Imágenes");
-    }
-  });
-
-  inputWords.addEventListener("click", function () {
-    if (inputWords.checked) {
-      alert("Seleccionaste Palabras");
-    }
-  });
-}
-seleccionaItemJugador();
 
 // mostrar las imagenes random al presionar el boton jugar. 
-let imagesArray = [];
-
+let container = document.getElementById("objects-container");
+let randomObjects
 document.getElementById("button-play").addEventListener("click", function () {
-  let container = document.getElementById("images-container");
-  for (let i = 0; i < 5; i++) {
-    let randomNumber = Math.floor(Math.random() * 1000) + 1;
-    let image = document.createElement("img");
-    image.src = "https://picsum.photos/200?image=" + randomNumber;
-    container.appendChild(image);
-    imagesArray.push(image);
+  randomObjects = [];
+  container.innerHTML = ""
+  let items = document.getElementsByName("items");
+  let selectedValue;
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].checked) {
+      selectedValue = items[i].id;
+      break;
+    }
+  }
+  if (selectedValue === "images") {
+    load("images")
+  } else if (selectedValue === "words") {
+    load("words")
   }
 });
 
+function load(type) {
+  
+  if (type == "images") {
+    //generar imagenes random
+    const n = 14;
+    
+    for (let i = 0; i < 5; i++) {
+      let randomNumber = Math.floor(Math.random() * n) + 1;
+      if (!randomObjects.includes(randomNumber)) {
+        let image = document.createElement("img");
+        image.src = "./states/image" + randomNumber + ".jpeg";
+        container.appendChild(image);
+        randomObjects.push(randomNumber);
+      } else {
+        i--;
+      }
+    }
+  } else {
+    // generar palabras random 
+    let text = "Amar Reir felicidad Retos Vivir Viajar Alegría Dormir Comer Hermoso Amigos Postres Desayuno Fiesta Montaña";
+    let someWords = text.split(" ");
+    for (let i = 0; i < 5; i++) {
+      var randomIndex = Math.floor(Math.random() * someWords.length);
+      if (!randomObjects.includes(someWords[randomIndex])) {
+        randomObjects.push(someWords[randomIndex]);
+      } else {
+        i--;
+      }
+    }
+    container.innerHTML = randomObjects.join(" | ");
+  }
+}
+
+
 // mostrar nuevamente las imagenes en desorden al presionar el boton emparejar. 
 
-let timerId;
+//let timerId;
 
-document.getElementById("button-emparejar").addEventListener("click", function () {
+function match() {
+  console.log("hola");
   let container = document.getElementById("images-container2");
   for (let i = 0; i < 5; i++) {
-    let randomIndex = Math.floor(Math.random() * imagesArray.length);
-    let image = imagesArray[randomIndex];
-    imagesArray.splice(randomIndex, 1);
+    let randomIndex = Math.floor(Math.random() * randomObjects.length);
+    let image = randomObjects[randomIndex];
+    randomObjects.splice(randomIndex, 1);
     container.appendChild(image);
 
     let input = document.createElement("input");
@@ -56,8 +81,8 @@ document.getElementById("button-emparejar").addEventListener("click", function (
       alert("Se te acabó el tiempo, perdiste.");
     }
   }, 1000);
-});
-
+}
+document.getElementById("button-match").addEventListener("click", match);
 
 document.getElementById("button-validate").addEventListener("click", function () {
   clearInterval(timerId);
@@ -102,4 +127,8 @@ document.getElementById("button-play").addEventListener("click", function () {
     order.push(images[i].src);
   }
   console.log("El orden de las imágenes después de hacer clic en el botón Jugar es:", order);
+});
+
+document.getElementById("button-reset").addEventListener("click", function () {
+  location.reload();
 });
